091c4eb61fb7fff9b5e1dfe28be11c037d961490
André Martins <andrebertonhadev@gmail.com>
2019-06-12 20:03:44 -0300

    frontend da api com react

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const app = express();
 
 const server = require('http').Server(app);
+// real time 
 const io = require('socket.io')(server);
 
 mongoose.connect('mongodb+srv://andrebertonha:andre@cluster0-kflaj.mongodb.net/test?retryWrites=true&w=majority', {
