2f0c5e84a9bb0fe5c6556657a8e5fe62ff01e48a
André Martins <andrebertonhadev@gmail.com>
2019-06-14 10:51:28 -0300

    eslint

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,20 +6,19 @@ const cors = require('cors');
 const app = express();
 
 const server = require('http').Server(app);
-// real time 
+// real time
 const io = require('socket.io')(server);
 
 mongoose.connect('mongodb+srv://andrebertonha:andre@cluster0-kflaj.mongodb.net/test?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
+  useNewUrlParser: true,
 });
 
 app.use((req, res, next) => {
-    req.io = io;
-
-    next();
+  req.io = io;
+  next();
 });
 
-//permitir que todo tipo de aplicacao acesse nosso backend
+// permitir que todo tipo de aplicacao acesse nosso back-end
 app.use(cors());
 
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
